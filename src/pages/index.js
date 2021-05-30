ff7c32e5e18450f67ad4278b8b3f830b06a820cb
Thay Giao Tien <thelaststanding97@gmail.com>
2021-05-30 12:57:59 +0300

    v1.1 Create Layout, get data by using RestAPI via getStaticProps method and creat Search Input

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Layout from '../components/Layout/Layout'
+import styles from '../styles/Home.module.css'
+import SearchInput from "../components/SearchInput/SearchInput";
+
+export default function Home({countries}) {
+  console.log({countries});                   // await function get the data first, then run this line
+  // we already created a props name {children} in Layout, so everything now we add to Layout is {children}
+  return <Layout>                             
+    <div className={styles.counts}> Found {countries.length} countries. </div>
+    <SearchInput placeholder="Filter by Name, Region or Sub Region" />
+  </Layout>;
+};
+
+export const getStaticProps= async()=>{  //nextJs will pre-render this page at build time using the props returned by this function
+  const res= await fetch("https://restcountries.eu/rest/v2/all");
+  const countries=  await res.json();
+  return {
+    props: {
+      countries,
+    },
+  };
+
+};
